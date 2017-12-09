d99a5ac0844e5cc50a8ad3b326823e57a0c24e96
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2017-12-09 00:13:56 -0200

    Added more domain errors and handled them at Gateway level

--- src/main/kotlin/br/ufs/kryptokarteira/backend/APIGateway.kt
+++ src/main/kotlin/br/ufs/kryptokarteira/backend/APIGateway.kt
@@ -1,12 +1,8 @@
 package br.ufs.kryptokarteira.backend
 
-import br.ufs.kryptokarteira.backend.domain.DomainError
-import br.ufs.kryptokarteira.backend.domain.ExternalServiceUnavailable
+import br.ufs.kryptokarteira.backend.domain.*
 import br.ufs.kryptokarteira.backend.services.BrokerService
-import spark.kotlin.exception
-import spark.kotlin.get
-import spark.kotlin.internalServerError
-import spark.kotlin.notFound
+import spark.kotlin.*
 
 class APIGateway(private val brokerService: BrokerService) {
 
@@ -20,32 +16,59 @@ class APIGateway(private val brokerService: BrokerService) {
         }
 
         notFound {
-            type(contentType = "application/json")
-            "{\"message\":\"Not Found\"}"
+            defaultRoutingWith(message = "Not found")
         }
 
         internalServerError {
-            type(contentType = "application/json")
-            "{\"message\":\"It`s not you. It`s us ... :sadpanda: \"}"
+            defaultRoutingWith(message = "Its not you ... Its us #sadpanda")
         }
 
         exception(DomainError::class, {
             when (exception) {
 
-                is ExternalServiceUnavailable -> {
-                    status(501)
-                    type(contentType = "application/json")
-                    response.body("{\"message\":\"Some internal system is unavailable\"}")
-                }
+                is ExternalServiceUnavailable ->
+                    failWith(
+                            statusCode = 502,
+                            errorMessage = "Some internal system is unavailable"
+                    )
+
+                is ExternalServiceTimeout ->
+                    failWith(
+                            statusCode = 504,
+                            errorMessage = "Received a timeout from an internal system"
+                    )
+
+                is ExternalServiceIntegrationError ->
+                    failWith(
+                            statusCode = 503,
+                            errorMessage = "Integration error with an internal system"
+                    )
+
+                is ExternalServiceContractError ->
+                    failWith(
+                            statusCode = 500,
+                            errorMessage = "Contract with an internal system is broken"
+                    )
 
                 else -> {
-                    internalServerError {
-                        type(contentType = "application/json")
-                        "{\"message\":\"It`s not you. It`s us ... :sadpanda: \"}"
-                    }
+                    failWith(
+                            statusCode = 500,
+                            errorMessage = "Its not you ... Its us #sadpanda"
+                    )
                 }
             }
         })
+    }
+
+    private fun RouteHandler.defaultRoutingWith(message: String): String {
+        type(contentType = "application/json")
+        return "{\"message\":\"$message\"}"
+    }
+
+    private fun ExceptionHandler.failWith(statusCode: Int, errorMessage: String) {
+        status(statusCode)
+        type(contentType = "application/json")
+        response.body("{\"message\":\"$errorMessage\"}")
     }
 
 }
