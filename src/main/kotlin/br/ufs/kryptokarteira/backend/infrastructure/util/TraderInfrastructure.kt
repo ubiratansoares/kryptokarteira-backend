968feef12a4783967c61df277ea341d33099ed6a
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2017-12-11 19:53:40 -0200

    Dumb impl. for TraderInfrastructure

--- src/main/kotlin/br/ufs/kryptokarteira/backend/infrastructure/util/TraderInfrastructure.kt
+++ src/main/kotlin/br/ufs/kryptokarteira/backend/infrastructure/util/TraderInfrastructure.kt
@@ -1,13 +1,13 @@
 package br.ufs.kryptokarteira.backend.infrastructure.util
 
 import br.ufs.kryptokarteira.backend.domain.CryptoCurrencyTrader
-import br.ufs.kryptokarteira.backend.domain.Transaction
 import br.ufs.kryptokarteira.backend.domain.DataForTransaction
+import br.ufs.kryptokarteira.backend.domain.Transaction
 
 class TraderInfrastructure : CryptoCurrencyTrader {
 
     override fun performTransaction(data: DataForTransaction): Transaction {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return Transaction("Success!")
     }
 
 }
